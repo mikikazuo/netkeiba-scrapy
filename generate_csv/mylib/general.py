9a1391f3e0861900677c8578402b843d3cb7895d
HOGE\81809 <booster515ezweb@gmail.com>
2023-03-23 22:00:00 +0900

    スクレイピングクラスの継承化

--- generate_csv/mylib/general.py
+++ generate_csv/mylib/general.py
@@ -2,6 +2,7 @@ import csv
 import os
 import pathlib
 import time
+from abc import abstractmethod
 from multiprocessing import Pool
 
 import lxml.html
@@ -37,14 +38,14 @@ def delete_space(result):
 
 
 class Scraper:
-    # @abstractmethod
-    # def scrape_from_page(self, race_html_path):
-    #     """
-    #     リストを戻り値とするスクレイピング関数、
-    #     """
-    #     pass
+    @abstractmethod
+    def scrape_from_page(self, race_html_path):
+        """
+        リストを戻り値とするスクレイピング関数、
+        """
+        pass
 
-    def __init__(self, input_html_dir, output_csv_path, scrape_from_page, regex: None | tuple | int = None):
+    def __init__(self, input_html_dir, output_csv_path, regex: None | tuple | int = None):
         """
         :param input_html_dir: データ抽出元となるhtmlのディレクトリ
         :param output_csv_path: csvの出力先パス
@@ -66,7 +67,6 @@ class Scraper:
             self.html_path_list = list(path.glob(str(regex) + '.html'))
 
         self.output_csv_path = output_csv_path
-        self.scrape_from_page = scrape_from_page
 
         self.csv_header = self.init_csv_header()
         self.exec_scraper()
@@ -90,27 +90,17 @@ class Scraper:
         """
         マルチプロセスによるスクレイピングの実行と結果の書き込み
         """
-
-        def write_result(path):
-            result_list = pool.map(self.scrape_from_page, path)
-            for result in result_list:
-                writer.writerows(result)
-
         start = time.time()
-
         with Pool() as pool:
             with open(self.output_csv_path, "a", encoding="utf-8") as csvfile:
                 writer = csv.DictWriter(csvfile, fieldnames=self.csv_header, lineterminator='\n')
                 # マルチプロセスで同時に参照するhtmlのパスリスト
                 pooled_path = []
-                for i, html_path in enumerate(tqdm(self.html_path_list)):
+                for html_path in tqdm(self.html_path_list):
                     pooled_path.append(html_path)
-                    # 一定数ごとにcsvに書き込み。
-                    # if ranker > 0 and ranker % os.cpu_count() == 0:
-                    #     write_result(pooled_path)
-                    #     pooled_path = []
-                # 終盤端数分の処理
-                write_result(pooled_path)
-
+                # imapは遅いためmapでまとめてスクレイピング
+                result_list = pool.map(self.scrape_from_page, pooled_path)
+                for result in result_list:
+                    writer.writerows(result)
         process_time = time.time() - start
         print(process_time)
--- generate_csv/scraper/race_scraper.py
+++ generate_csv/scraper/race_scraper.py
@@ -2,47 +2,49 @@ from generate_csv import mylib
 from netkeiba.netkeiba.spiders import mylib as crawl_mylib
 
 
-def scrape_from_page(race_html_path):
-    html = mylib.read_html(race_html_path)
+class RaceScraper(mylib.Scraper):
+    def scrape_from_page(self, race_html_path):
+        html = mylib.read_html(race_html_path)
 
-    result_table_rows = html.xpath('//table[@class="race_table_01 nk_tb_common"]//tr[position()>1]')
-    race_result_all = []
-    for result_table_row in result_table_rows:
-        owner = result_table_row.xpath("diary_snap_cut/td/a/@title")
-        # 例外処理　URLリンクがついていないパターンがある
-        if len(owner):
-            owner = owner[0]
-            owner_id = crawl_mylib.get_last_slash_word(result_table_row.xpath("diary_snap_cut/td/a/@href")[-1])
-        else:
-            owner = result_table_row.xpath("diary_snap_cut/td//text()")[-1].strip()
-            owner_id = None
+        result_table_rows = html.xpath('//table[@class="race_table_01 nk_tb_common"]//tr[position()>1]')
+        race_result_all = []
 
-        """
-        【注意】ファイルから取得する場合は、一部tdがdiary_snap_cutで囲われているためtdの要素数が一致しない
-               ページ表示中はdiary_snap_cutで囲われなくなる。                    
-        """
-        race_result = {
-            "race_id": race_html_path.stem,
-            "horse_id": result_table_row.xpath("td[4]/a/@href")[0][7:-1],
-            "sex": result_table_row.xpath("td[5]")[0].text[0],
-            "age": result_table_row.xpath("td[5]")[0].text[1:],
-            "tresen": result_table_row.xpath("td[13]")[0].text.strip()[1],  # 前後に多数の空白があるためstrip処理 ex.200006060207
-            "trainer": result_table_row.xpath("td[13]/a/@title")[0],
-            "trainer_id": crawl_mylib.get_last_slash_word(result_table_row.xpath("td[13]/a/@href")[0]),
-            "owner": owner,  # 馬主は途中で変わることがある
-            "owner_id": owner_id,
-        }
+        for result_table_row in result_table_rows:
+            owner = result_table_row.xpath("diary_snap_cut/td/a/@title")
+            # 例外処理　URLリンクがついていないパターンがある
+            if len(owner):
+                owner = owner[0]
+                owner_id = crawl_mylib.get_last_slash_word(result_table_row.xpath("diary_snap_cut/td/a/@href")[-1])
+            else:
+                owner = result_table_row.xpath("diary_snap_cut/td//text()")[-1].strip()
+                owner_id = None
 
-        race_info = html.xpath("//diary_snap_cut/span")[0].text.split("/")
-        race_result["turn"] = None  # Noneのパターンあり
-        for i in ["左", "右", "直"]:
-            if i in race_info[0]:
-                race_result["turn"] = i
-                break
-        race_result["outside"] = "外" in race_info[0]
-        race_result["start_time"] = race_info[-1].split(" : ", 1)[-1]
-        race_result_all.append(race_result)
-    return race_result_all
+            """
+            【注意】ファイルから取得する場合は、一部tdがdiary_snap_cutで囲われているためtdの要素数が一致しない
+                   ページ表示中はdiary_snap_cutで囲われなくなる。                    
+            """
+            race_result = {
+                "race_id": race_html_path.stem,
+                "horse_id": result_table_row.xpath("td[4]/a/@href")[0][7:-1],
+                "sex": result_table_row.xpath("td[5]")[0].text[0],
+                "age": result_table_row.xpath("td[5]")[0].text[1:],
+                "tresen": result_table_row.xpath("td[13]")[0].text.strip()[1],  # 前後に多数の空白があるためstrip処理 ex.200006060207
+                "trainer": result_table_row.xpath("td[13]/a/@title")[0],
+                "trainer_id": crawl_mylib.get_last_slash_word(result_table_row.xpath("td[13]/a/@href")[0]),
+                "owner": owner,  # 馬主は途中で変わることがある
+                "owner_id": owner_id,
+            }
+
+            race_info = html.xpath("//diary_snap_cut/span")[0].text.split("/")
+            race_result["turn"] = None  # Noneのパターンあり
+            for i in ["左", "右", "直"]:
+                if i in race_info[0]:
+                    race_result["turn"] = i
+                    break
+            race_result["outside"] = "外" in race_info[0]
+            race_result["start_time"] = race_info[-1].split(" : ", 1)[-1]
+            race_result_all.append(race_result)
+        return race_result_all
 
 
 if __name__ == '__main__':
@@ -50,4 +52,4 @@ if __name__ == '__main__':
     # スクレイピング結果csvの出力先パス
     output_csv_path = "D:/netkeiba/csv_data/race.csv"
 
-    mylib.Scraper(input_html_dir, output_csv_path, scrape_from_page, (2018, 2030))
+    RaceScraper(input_html_dir, output_csv_path, (2017, 2030))
