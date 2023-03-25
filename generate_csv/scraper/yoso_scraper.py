fc2992a7fd03b41a844f098651ec616db8718633
HOGE\81809 <booster515ezweb@gmail.com>
2023-03-25 11:24:51 +0900

    【スクレイピング（予想）】新規追加

---
+++ generate_csv/scraper/yoso_scraper.py
@@ -0,0 +1,40 @@
+from generate_csv import mylib
+from netkeiba.netkeiba.spiders import mylib as crawl_mylib
+
+
+class YosoScraper(mylib.Scraper):
+    def scrape_from_page(self, html_path):
+        html = mylib.read_html(html_path)
+
+        result_table_rows = html.xpath('//div[@class="YosoDeTailList"]')
+        result_all = []
+        for result_table_row in result_table_rows:
+            yosoka = result_table_row.xpath('div/div/p[@class="YosoDeTailName"]')[0].text
+            yoso_rows = result_table_row.xpath('div[contains(@class,"YosoDeTailItem2")]/table/tbody/tr')
+            for yoso_row in yoso_rows:
+                icon_name = ['Honmei', 'Taikou', 'Kurosan', 'Hoshi', 'Osae']
+                yoso = None
+                for name in icon_name:
+                    if yoso_row.xpath(f'th/span[contains(@class,"Icon_{name}")]'):
+                        yoso = name
+                        break
+                horse_id = crawl_mylib.get_last_slash_word(yoso_row.xpath('td/a/@href')[0])
+
+                result = {
+                    "race_id": html_path.stem,
+                    "horse_id": horse_id,
+                    "yosoka": yosoka,
+                    "yoso": yoso
+                }
+
+                result_all.append(result)
+        return result_all
+
+
+if __name__ == '__main__':
+    input_html_dir = "D:/netkeiba/html_data/yoso/"
+    # スクレイピング結果csvの出力先パス
+    output_csv_path = "D:/netkeiba/csv_data/yoso.csv"
+
+    # race_scraper.pyの取得範囲と合わせる
+    YosoScraper(input_html_dir, output_csv_path, (2017, 2030))
