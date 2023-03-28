dedc798b14b6acbb183f2ac9ab51936b6ebc7c9a
mikikazuo <ers7m3b@gmail.com>
2023-03-28 22:46:02 +0900

    【csv読み取り（予想）】新規追加

---
+++ analysis/mylib/yoso_processing.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from tqdm import tqdm
+
+import mypath
+from analysis.mylib.general import *
+
+
+class YosoProcessing(DataframeProcessing):
+    yoso_columns = []
+
+    def __init__(self):
+        # 海外のレースが含まれない範囲の場合、int型になってしまうため手動でstr化 ex.2019J0033009
+        self.df = pd.read_csv(mypath.yoso_csv, dtype={'race_id': str}, index_col=["race_id", "horse_id"])
+
+        print(f"予想家index: {self.df['yosoka'].unique()}")
+        merged_df = None
+        for index, yosoka in enumerate(self.df['yosoka'].unique()):
+            filtered_yosoka_df = self.df.loc[self.df['yosoka'] == yosoka]
+            filtered_yosoka_df = filtered_yosoka_df.drop('yosoka', axis=1)
+            filtered_yosoka_df = filtered_yosoka_df.rename(columns={'yoso': f'yoso_d{index + 1}'})
+            self.yoso_columns.append(f'yoso_d{index + 1}')
+            merged_df = filtered_yosoka_df if merged_df is None else merged_df.merge(filtered_yosoka_df,
+                                                                                     on=["race_id", "horse_id"],
+                                                                                     how='outer')
+        self.df = merged_df
+
+    def categorize(self, merged_df):
+        """
+        元々予想家がいなかったのか外れ予想なのかを区別し、dtypeを変更する
+        """
+        categorized_df = merged_df
+        print('yoso categorizing')
+        for race_id, df in tqdm(merged_df.groupby("race_id")):
+            for column in self.yoso_columns:
+                categorized_df.loc[race_id, column] = df[column].fillna(
+                    'None' if categorized_df.loc[race_id, column].isnull().all() else 'Hazure')
+
+        for col in self.yoso_columns:
+            categorized_df[col] = categorized_df[col].astype("category")
+        return categorized_df
