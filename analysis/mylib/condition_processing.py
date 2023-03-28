de864ed3268286ef411e1d330895c8c234da7a5c
mikikazuo <ers7m3b@gmail.com>
2023-03-28 22:44:34 +0900

    【csv読み取り（調子偏差値）】新規追加

---
+++ analysis/mylib/condition_processing.py
@@ -0,0 +1,24 @@
+import ast
+
+import pandas as pd
+
+import mypath
+from analysis.mylib.general import *
+
+
+class ConditionProcessing(DataframeProcessing):
+    def __init__(self):
+        # 海外のレースが含まれない範囲の場合、int型になってしまうため手動でstr化 ex.2019J0033009
+        self.df = pd.read_csv(mypath.condition_csv, dtype={'race_id': str})
+
+        self.df['condition_score_past'] = self.df['condition_score_past'].map(ast.literal_eval)
+        # サイト直で過去データを取得した場合はカラム名「_d」の後にインデックスをつける
+        for i in range(5):
+            self.df[f'condition_score_d{str(i + 1)}'] = self.df['condition_score_past']
+            self.df[f'condition_score_d{str(i + 1)}'] = self.df[f'condition_score_d{str(i + 1)}'].map(
+                lambda x: x[i] if len(x) > i else -100)
+            self.change_type([f'condition_score_d{str(i + 1)}'], "int8")
+        self.df = self.df.drop('condition_score_past', axis=1)
+
+        self.change_type(['condition_score', 'condition_rank', 'rise_rank'], "int8")
+        self.df = self.df.set_index(["race_id", "horse_id"])
